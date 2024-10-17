13719af55610aa3d2a8d6c681e91fb3c5465421c
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-10-17 08:09:48 +0300

    add colors constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+Color lightOrange = const Color(0xffFFC677);
+Color lightBlue = const Color(0xff77DDFF);
+Color lightWhite = const Color(0xffEAECFF);
+Color darkGrey = const Color(0xff2D2F41);
+Color lightBlueishPurple = const Color(0xff748EF6);
+Color lightPurple = const Color(0xffC279FB);
+Color pink = const Color(0xffEA74AB);
+Color mediumGrey = const Color(0xff707070);
+Color darkBlue = const Color(0xff444974);
+
+final List<List<Color>> gradientColors = [
+  [const Color(0xFF6448FE), const Color(0xFF5FC6FF)],
+  [const Color(0xFFFE6197), const Color(0xFFFFB463)],
+  [const Color(0xFF61A3FE), const Color(0xFF63FFD5)],
+  [const Color(0xFFFFA738), const Color(0xFFFFE130)],
+  [const Color(0xFFFF5DCD), const Color(0xFFFF8484)],
+];
