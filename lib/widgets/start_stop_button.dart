27f3122ffad0cbb7ac853ced67fe0c854551483e
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-10-22 15:40:16 +0300

    create start stop button widget

---
+++ lib/widgets/start_stop_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:time_tide/constants/colors.dart';
+
+class StartStopButton extends StatelessWidget {
+  const StartStopButton(
+      {super.key, required this.onPressed, required this.isRunning});
+  final VoidCallback onPressed;
+  final bool isRunning;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton(
+      child: Container(
+        alignment: Alignment.center,
+        width: 250,
+        height: 250,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(300),
+          border: Border.all(
+            width: 5,
+            color: isRunning ? Colors.red : lightPurple,
+          ),
+        ),
+        child: Text(
+          isRunning ? "Stop" : "Start",
+          style: TextStyle(
+            fontFamily: 'avenir',
+            fontSize: 38,
+            fontWeight: FontWeight.bold,
+            color: lightWhite,
+          ),
+        ),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
