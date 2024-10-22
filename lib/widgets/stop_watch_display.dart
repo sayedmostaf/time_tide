bdd983791699377e361513b812995730184d7da0
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-10-22 15:34:38 +0300

    create stop watch display

---
+++ lib/widgets/stop_watch_display.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:time_tide/constants/colors.dart';
+
+class StopWatchDisplay extends StatelessWidget {
+  const StopWatchDisplay(
+      {super.key, required this.displayTime, required this.onPressed});
+  final String displayTime;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton(
+      child: Container(
+        alignment: Alignment.center,
+        width: 250,
+        height: 250,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(300),
+          border: Border.all(
+            width: 5,
+            color: lightPurple,
+          ),
+        ),
+        child: Text(
+          displayTime,
+          style: TextStyle(
+            fontFamily: 'avenir',
+            fontSize: 38,
+            fontWeight: FontWeight.bold,
+            color: lightWhite,
+          ),
+        ),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
