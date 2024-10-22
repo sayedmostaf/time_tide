2d17873dd27ff6962d76a1fe24f4a4f846176767
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-10-22 20:30:11 +0300

    create alarm switcher widget

---
+++ lib/widgets/alarm_switcher.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:time_tide/constants/colors.dart';
+import 'package:time_tide/models/alarm_model.dart';
+
+class AlarmSwitcher extends StatefulWidget {
+  AlarmSwitcher(this.onOff, this.timestamp, this.id, {super.key});
+  bool onOff;
+  Timestamp timestamp;
+  String id;
+  @override
+  State<AlarmSwitcher> createState() => _AlarmSwitcherState();
+}
+
+class _AlarmSwitcherState extends State<AlarmSwitcher> {
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      activeColor: Colors.white,
+      inactiveThumbColor: mediumGrey,
+      inactiveTrackColor: mediumGrey.withOpacity(.5),
+      value: widget.onOff,
+      onChanged: (bool value) {
+        AlarmModel alarmModel = AlarmModel();
+        alarmModel.onOff = value;
+        alarmModel.timestamp = widget.timestamp;
+        // FirebaseFirestore.instance
+        //     .collection('alarm')
+        //     .doc(widget.id)
+        //     .update(alarmModel.toJson());
+      },
+    );
+  }
+}
